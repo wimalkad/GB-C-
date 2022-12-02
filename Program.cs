30e6468b5e55e69213fbf29839f080e7b3c4af04
Danil Zharikov <danil.zharikov@list.ru>
2022-12-02 05:55:45 +0300

    Added project to git

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+/*Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+Console.WriteLine("Число " + a + " большее" + ", а число " + b + " меньшее");
+else
+Console.WriteLine("Число " + b + " большее" + ", а число " + a + " меньшее"); */
+
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+/* Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное число :" + max);*/
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+/*Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+/*Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N>0)
+{
+    int i = 1;
+    Console.WriteLine("Все четные числа до N: ");
+    while (i<N)
+{
+        if (i % 2 == 0)
+    {
+            Console.WriteLine(i);
+    }
+    i++;
+}
+}*/
